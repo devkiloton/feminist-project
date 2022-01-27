7d7efef85190d2a57339e2530daad20537bc24b4
Yarego Brozek <yarego.brozek@lucaspaganini.com>
2022-01-26 23:31:37 -0300

    creating header & lazy loading

---
+++ home-routing.module.ts
@@ -0,0 +1,28 @@
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+    children:[
+      {
+        path:'',
+        component:LoginComponent
+      },
+      {
+        path: 'signup',
+        component: SignUpComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
