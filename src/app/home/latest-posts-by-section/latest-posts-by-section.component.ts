7b19ed03ee87439c86e05dbc5c5742bb2017471b
Yarego Brozek <yarego.brozek@lucaspaganini.com>
2022-01-30 23:40:19 -0300

    Starting to create carousels in th home page

--- src/app/home/latest-posts-by-section/latest-posts-by-section.component.ts
+++ src/app/home/latest-posts-by-section/latest-posts-by-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-latest-posts-by-section',
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LatestPostsBySectionComponent implements OnInit {
 
+  mocks: any = require('../../../Mocks/latest-posts-by-section.json');
   constructor() { }
-
+  @Input() section:string = '';
   ngOnInit(): void {
   }
 
--- src/app/home/latest-posts-by-section/post/post.component.ts
+++ src/app/home/latest-posts-by-section/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class PostComponent implements OnInit {
 
   constructor() { }
-
+  @Input() title = '';
+  @Input() thumb = ''
   ngOnInit(): void {
   }
 
