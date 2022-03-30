bc932295334fd0b97dbbaf6d1951e82f3160d57f
Yarego Brozek <devkiloton@gmail.com>
2022-03-30 01:01:17 -0300

    Styling grid

--- src/app/shared/latest-posts/latest-posts.component.ts
+++ src/app/shared/latest-posts/latest-posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-latest-posts',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LatestPostsComponent implements OnInit {
 
+  @Input() mocks = require('../../../Mocks/latest-posts-by-section.json');
+  images = [this.mocks[0].image,this.mocks[1].image,this.mocks[2].image,this.mocks[3].image];
+  titles = [this.mocks[0].title,this.mocks[1].title,this.mocks[2].title,this.mocks[3].title];
   constructor() { }
 
   ngOnInit(): void {
